da486bffb828bf47c1ed57eae9c01c5166ead6cd
aembadawy <aem.badawy@gmail.com>
2025-05-14 19:21:15 +0300

    Arrays

--- FindMaxInArr.playground/Contents.swift
+++ FindMaxInArr.playground/Contents.swift
@@ -1,3 +1,33 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let nums: [Int] = [1,3,2,5,7,6,9,12,5,19,8]
+
+func getMax(_ nums: [Int]) -> Int {
+    var max = 0
+    for n in nums {
+        max = n > max ? n : max
+    }
+    return max
+}
+getMax(nums)
+
+func getMin(_ nums: [Int]) -> Int {
+    var min = Int.max
+    for n in nums {
+        min = n < min ? n : min
+    }
+    return min
+}
+getMin(nums)
+
+let numsWithNegatives: [Int] = [1,-3,2,5,7,6,-9,12,5,-19,8]
+
+func getMaxFromArrayWithNegative(_ nums: [Int]) -> Int {
+    var max = Int.min
+    for n in nums {
+        max = n > max ? n : max
+    }
+    return max
+}
+
+getMaxFromArrayWithNegative(numsWithNegatives)
