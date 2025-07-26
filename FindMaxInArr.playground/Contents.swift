5275ecb63080bba2e00efdf8f1a43fd02240a67b
Aya Badawy <aem.badawy@gmail.com>
2025-07-26 22:09:16 +0300

    Update Contents.swift

--- FindMaxInArr.playground/Contents.swift
+++ FindMaxInArr.playground/Contents.swift
@@ -227,6 +227,25 @@ class HitCounter {
     }
 }
 
+func addStrings(_ num1: String, _ num2: String) -> String {
+        
+    let nums1 = Array(num1), nums2 = Array(num2)
+    var i = nums1.count - 1, j = nums2.count - 1
+    var carry = 0
+    var result = ""
+
+    while i >= 0 || j >= 0 || carry > 0 {
+        let digit1 = i >= 0 ? Int(String(nums1[i]))! : 0
+        let digit2 = j >= 0 ? Int(String(nums2[j]))! : 0
+        let sum = digit1 + digit2 + carry
+        carry = sum / 10
+        result = String(sum % 10) + result
+        i -= 1
+        j -= 1
+    }
+
+    return result
+    }
 
 /**
  * Your HitCounter object will be instantiated and called as such:
