96ce8eb31e06c0545727bcc0462aa5c0c4ea7767
aembadawy <aem.badawy@gmail.com>
2025-07-31 09:02:42 +0300

    283. Move Zeroes

--- TowPointers.playground/Contents.swift
+++ TowPointers.playground/Contents.swift
@@ -1,2 +1,14 @@
 import Foundation
 
+// 283. Move Zeroes
+func moveZeroes(_ nums: inout [Int]) {
+    var l = 0
+    for r in 0..<nums.count {
+        if nums[r] != 0  {
+            nums.swapAt(l, r)
+            l += 1
+        }
+    }
+}
+var grid = [0,1,0,3,12]
+moveZeroes(&grid)
