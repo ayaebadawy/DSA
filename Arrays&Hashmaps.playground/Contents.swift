3f47137dff1031029df97932c9af5e381af9c76d
aembadawy <aem.badawy@gmail.com>
2025-08-01 21:05:45 +0300

    1004. Max Consecutive Ones III

--- Arrays&Hashmaps.playground/Contents.swift
+++ Arrays&Hashmaps.playground/Contents.swift
@@ -1,2 +1,3 @@
 import Foundation
 
+Array(arrayLiteral: 1,3,4,5,8)
--- SlidingWindow.playground/Contents.swift
+++ SlidingWindow.playground/Contents.swift
@@ -1,2 +1,17 @@
 import Foundation
 
+//1004. Max Consecutive Ones III
+func longestOnes(_ nums: [Int], _ k: Int) -> Int {
+    var maxWindow = 0, numOfZeros = 0 , l = 0
+    for r in 0..<nums.count {
+        if nums[r] == 0 { numOfZeros += 1 }
+        while numOfZeros > k {
+            if nums[l] == 0 {
+                numOfZeros -= 1
+            }
+            l += 1
+        }
+        maxWindow = max(maxWindow, (r-l+1))
+    }
+    return maxWindow
+}
