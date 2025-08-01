25778af6466897a06dfcfeaba0cfcfd606aed97e
aembadawy <aem.badawy@gmail.com>
2025-08-01 07:48:09 +0300

    338. Counting Bits

--- DP.playground/Contents.swift
+++ DP.playground/Contents.swift
@@ -1,2 +1,15 @@
 import Foundation
 
+// 338. Counting Bits
+//https://leetcode.com/problems/counting-bits/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+func countBits(_ n: Int) -> [Int] {
+    var dp = Array(repeating: 0, count: n + 1)
+    var offset = 1
+    for i in 1..<n+1 {
+        if offset * 2 == i { offset = i }
+        dp[i] = 1 + dp[i - offset]
+    }
+    return dp
+}
+
+countBits(5)
