0ab6e2ad4a034ad6a69106331dde7602ee5bb671
aembadawy <aem.badawy@gmail.com>
2025-08-07 21:33:19 +0300

    242. Valid Anagram

--- Arrays&Hashmaps.playground/Contents.swift
+++ Arrays&Hashmaps.playground/Contents.swift
@@ -10,3 +10,18 @@ func containsDuplicate(_ nums: [Int]) -> Bool {
     }
     return false
 }
+containsDuplicate([1,2,3,1])
+
+//242. Valid Anagram
+//https://leetcode.com/problems/valid-anagram/description/
+func isAnagram(_ s: String, _ t: String) -> Bool {
+    var sDict = [Character: Int](), tDict = [Character: Int]()
+    for char in s {
+        sDict[char, default: 0] += 1
+    }
+    for char in t {
+        tDict[char, default: 0] += 1
+    }
+    return sDict == tDict
+}
+isAnagram("anagram", "nagaram")
