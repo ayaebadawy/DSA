744a253d435d33d8765d2481dafc97c0b28241f8
ayaebadawy <ayaebadawy@gmail.com>
2025-08-11 16:47:24 +0300

    Backtracking explaination

--- Backtracking.playground/Contents.swift
+++ Backtracking.playground/Contents.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+/*
+backtracting is walking a maze:
+At each junction, you choose a valid direction and go forward.
+Once you reach the end (a base case), you walk back to the last junction and try the next unexplored path.
+You don’t go back and re-walk the same path you already finished.
+*/
+
 //22. Generate Parentheses Time(2^n) beacuse of how the tree in built Space O(n) the call stack
 //Stack + backtracking problem
 //https://leetcode.com/problems/generate-parentheses/description/
@@ -19,3 +26,15 @@ func generateParenthesis(_ n: Int) -> [String] {
         return res
     }
 }
+generateParenthesis(2)
+
+/*
+ backtrack(0, 0, 2, "")
+ ├── backtrack(1, 0, 2, "(")
+ │   ├── backtrack(2, 0, 2, "((")
+ │   │   └── backtrack(2, 1, 2, "(()")
+ │   │       └── backtrack(2, 2, 2, "(())")  ✅ base case → append "(())"
+ │   └── backtrack(1, 1, 2, "()")
+ │       └── backtrack(2, 1, 2, "()(")
+ │           └── backtrack(2, 2, 2, "()()") ✅ base case → append "()()"
+ */
