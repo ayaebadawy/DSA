d1cbf8a7d25a4fb74026a965f3a9b594ce6fc2c1
ayaebadawy <ayaebadawy@gmail.com>
2025-09-02 21:05:25 +0300

    3. Longest Substring Without Repeating Characters

--- SlidingWindow.playground/Contents.swift
+++ SlidingWindow.playground/Contents.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 //1004. Max Consecutive Ones III
+//https://leetcode.com/problems/max-consecutive-ones-iii/description/
 func longestOnes(_ nums: [Int], _ k: Int) -> Int {
     var maxWindow = 0, numOfZeros = 0 , l = 0
     for r in 0..<nums.count {
@@ -12,6 +13,22 @@ func longestOnes(_ nums: [Int], _ k: Int) -> Int {
             l += 1
         }
         maxWindow = max(maxWindow, (r-l+1))
+    }
+    return maxWindow
+}
+
+//3. Longest Substring Without Repeating Characters
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    var maxWindow = 0, l = 0, used = Set<Character>()
+    let s = Array(s)
+    for r in 0..<s.count {
+        while used.contains(s[r]) { //invalid window, contract
+            used.remove(s[l])
+            l += 1
+        }
+        used.insert(s[r])
+        maxWindow = max(maxWindow, r-l+1)
     }
     return maxWindow
 }
