66614388916c08531882ab62a2daef79245e2703
ayaebadawy <ayaebadawy@gmail.com>
2025-09-10 21:23:09 +0300

    26. Remove Duplicates from Sorted Array

--- TowPointers.playground/Contents.swift
+++ TowPointers.playground/Contents.swift
@@ -120,3 +120,16 @@ func intervalIntersection(_ firstList: [[Int]], _ secondList: [[Int]]) -> [[Int]
     }
     return res
 }
+
+//26. Remove Duplicates from Sorted Array
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+    var l = 1
+    for r in 1..<nums.count{
+        if nums[r] != nums[r-1] {
+            nums[l] = nums[r]
+            l += 1
+        }
+    }
+    return l
+}
