6221066905c59d27e26c14ea17ea779067132fab
ayaebadawy <ayaebadawy@gmail.com>
2025-09-13 09:11:28 +0300

    74. Search a 2D Matrix

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -21,9 +21,30 @@ func search(_ nums: [Int], _ target: Int) -> Int {
     var l = 0, r = nums.count - 1
     while l <= r {
         let  mid = l + ((r-l) / 2)
-        if nums[mid] > target { r -= 1 }
-        else if nums[mid] < target { l += 1}
-        else { return mid}
+        if nums[mid] > target { r = mid - 1 }
+        else if nums[mid] < target { l = mid + 1 }
+        else { return mid }
     }
     return -1
+}
+
+// 74. Search a 2D Matrix    Staircase Search   Time O(m+n)
+// Another solution would be to flatten the 2D matrix and then run binary search
+// https://leetcode.com/problems/search-a-2d-matrix/description/
+func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    var col = 0, row = matrix.count - 1
+    var inBounds: Bool {
+        return row >= 0 && row < matrix.count
+        && col >= 0 && col < matrix[0].count
+    }
+    while inBounds {
+        if matrix[row][col] > target {
+            row -= 1
+        } else if matrix[row][col] < target {
+            col += 1
+        } else {
+            return true
+        }
+    }
+    return false
 }
