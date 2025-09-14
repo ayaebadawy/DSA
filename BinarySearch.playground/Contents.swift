4c1f87a1adb063363d2fc584187134584f8d9621
ayaebadawy <ayaebadawy@gmail.com>
2025-09-14 22:47:56 +0300

    33. Search in Rotated Sorted Array

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -48,3 +48,36 @@ func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
     }
     return false
 }
+
+// 33. Search in Rotated Sorted Array
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+func searchRotatedSortedArray(_ nums: [Int], _ target: Int) -> Int {
+    var left = 0
+    var right = nums.count - 1
+    
+    while left <= right {
+        let mid = (left + right) / 2
+        
+        if nums[mid] == target {
+            return mid
+        }
+        
+        // Check if the left half is sorted
+        if nums[left] <= nums[mid] {
+            if nums[left] <= target && target < nums[mid] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        // Otherwise, the right half must be sorted
+        else {
+            if nums[mid] < target && target <= nums[right] {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+    }
+    return -1
+}
