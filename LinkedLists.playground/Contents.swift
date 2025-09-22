888b29e58b8c87ba54c3ec409b7779826aae76d2
ayaebadawy <ayaebadawy@gmail.com>
2025-09-22 09:21:14 +0300

    206. Reverse Linked List

--- LinkedLists.playground/Contents.swift
+++ LinkedLists.playground/Contents.swift
@@ -43,3 +43,24 @@ func insert(_ head: Node?, _ insertVal: Int) -> Node? {
     cur.next = newNode
     return head
 }
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+//206. Reverse Linked List     Time O(n)
+//https://leetcode.com/problems/reverse-linked-list/description/
+func reverseList(_ head: ListNode?) -> ListNode? {
+    var prev: ListNode? = nil, curr = head
+    while curr != nil {
+        let nxt = curr?.next // temp var to save the next node before breaking the link
+        curr?.next = prev
+        prev = curr
+        curr = nxt
+    }
+    return prev
+}
