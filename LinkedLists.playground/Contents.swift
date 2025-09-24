a8ca1976c89b1c0d7ad197ad61ba4ebf1c62efcf
ayaebadawy <ayaebadawy@gmail.com>
2025-09-24 11:55:06 +0300

    287. Find the Duplicate Number

--- LinkedLists.playground/Contents.swift
+++ LinkedLists.playground/Contents.swift
@@ -153,3 +153,21 @@ func pairSum(_ head: ListNode?) -> Int {
     }
     return res
 }
+
+//287. Find the Duplicate Number
+//https://leetcode.com/problems/find-the-duplicate-number/description/
+
+func findDuplicate(_ nums: [Int]) -> Int {
+    var fast = 0, slow = 0
+    while true {
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if fast == slow { break }
+    }
+    var slow2 = 0
+    while true {
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if slow == slow2 { return slow}
+    }
+}
