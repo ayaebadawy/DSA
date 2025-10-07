ac0b8ff082a53dcfebafb1cfbf33abcd30b11e20
ayaebadawy <ayaebadawy@gmail.com>
2025-10-07 20:17:53 +0300

    70. Climbing Stairs

--- Math.playground/Contents.swift
+++ Math.playground/Contents.swift
@@ -1,2 +1,14 @@
 import Foundation
 
+//70. Climbing Stairs    fibinacci series
+//https://leetcode.com/problems/climbing-stairs/description/
+func climbStairs(_ n: Int) -> Int {
+    if n <= 2 { return n }
+    var first = 1, sec = 2
+    for _ in 3...n {
+        let third = first + sec
+        first = sec
+        sec = third
+    }
+    return sec
+}
