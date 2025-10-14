be86f25ca45a4f8ffb400319c5f3ef7b2fd2f734
ayaebadawy <ayaebadawy@gmail.com>
2025-10-14 18:24:35 +0300

    50. Pow(x, n) 66. Plus One 202. Happy Number

--- Math.playground/Contents.swift
+++ Math.playground/Contents.swift
@@ -101,3 +101,62 @@ class DetectSquares {
         return res
     }
 }
+
+//50. Pow(x, n)
+//https://leetcode.com/problems/powx-n/submissions/1800668985/
+func myPow(_ x: Double, _ n: Int) -> Double {
+    if x == 0 { return 0 } // 0^ anything is 0
+    if n == 0 { return 1 } //anything^0 = 1
+    var res = 1.0
+    for _ in 0..<abs(n) {
+        res *= x
+    }
+    return n > 0 ? res : 1 / res
+}
+
+func myPowRecrsive(_ x: Double, _ n: Int) -> Double {
+    if x == 0 { return 0 } //basecase
+    if n == 0 { return 1 } //basecase
+    var res = myPow(x * x, abs(n) / 2)
+    if abs(n) % 2 != 0 { res = res * x }
+    if n < 0 { res = 1 / res }
+    return res
+}
+
+//66. Plus One
+//https://leetcode.com/problems/plus-one/description/
+func plusOne(_ digits: [Int]) -> [Int] {
+    var digits = digits
+    
+    for i in stride(from: digits.count - 1, through: 0, by: -1) {
+        if digits[i] < 9 {
+            digits[i] += 1
+            return digits
+        } else {
+            digits[i] = 0
+        }
+    }
+    return [1] + digits // if return inside loop never excutes then we have a carry of 1
+}
+
+//202. Happy Number
+//https://leetcode.com/problems/happy-number/
+func isHappy(_ n: Int) -> Bool {
+    var visit = Set<Int>(), n = n
+    while !visit.contains(n) {
+        visit.insert(n)
+        n = sumOfSqr(n) //update n
+        if n == 1 { return true }
+    }
+    return false
+    
+    func sumOfSqr(_ n: Int) -> Int {
+        var output = 0, n = n
+        while n != 0 {
+            let digi = n % 10 //gets the first digit in 19 it is 9
+            output += (digi * digi)
+            n = n / 10 //remove the digit we just processed 19 it is 1
+        }
+        return output
+    }
+}
