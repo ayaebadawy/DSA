2161fada286375253a9236c817243c2451dc6714
ayaebadawy <ayaebadawy@gmail.com>
2025-10-17 20:58:17 +0300

    Remove

--- Math.playground/Contents.swift
+++ Math.playground/Contents.swift
@@ -232,15 +232,3 @@ func setZeroes(_ matrix: inout [[Int]]) {
         }
     }
 }
-
-func majorityElement(_ nums: [Int]) -> Int {
-    var candidate = nums[0], count = 1
-    for i in 1..<nums.count {
-        if count == 0 { candidate = nums[i] }
-        if nums[i] == candidate { count += 1}
-        else {
-            count -= 1
-        }
-    }
-    return candidate
-}
